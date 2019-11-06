5034263da1be379b0051aeaeeba32284ff0595c9
Tim Bolzern <t.bolzern@gmail.com>
2019-11-06 19:10:35 +0100

    removed group ranking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { TeamDetailComponent } from './team-detail/team-detail.component';
 import { GroupListComponent } from './group-list/group-list.component';
 import { GroupDetailComponent } from './group-detail/group-detail.component';
 import { GroupGamesComponent } from './group-games/group-games.component';
-import { GroupRankingComponent } from './group-ranking/group-ranking.component';
 import { GroupStageGamesComponent } from './group-stage-games/group-stage-games.component';
 import { SecondGroupStageGamesComponent } from './second-group-stage-games/second-group-stage-games.component';
 import { FinalGamesComponent } from './final-games/final-games.component';
@@ -33,7 +32,6 @@ import { FinalGamesComponent } from './final-games/final-games.component';
     GroupListComponent,
     GroupDetailComponent,
     GroupGamesComponent,
-    GroupRankingComponent,
     GroupStageGamesComponent,
     SecondGroupStageGamesComponent,
     FinalGamesComponent
