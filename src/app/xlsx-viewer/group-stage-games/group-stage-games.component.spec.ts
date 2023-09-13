c07e161c5a65e0b73b0230485768538cbd4f0f54
Tim Bolzern <tbo@bica.ch>
2023-09-13 10:10:50 +0200

    Upgrades to ng11

--- src/app/xlsx-viewer/group-stage-games/group-stage-games.component.spec.ts
+++ src/app/xlsx-viewer/group-stage-games/group-stage-games.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GroupStageGamesComponent } from './group-stage-games.component';
 
@@ -6,7 +6,7 @@ describe('GroupStageGamesComponent', () => {
   let component: GroupStageGamesComponent;
   let fixture: ComponentFixture<GroupStageGamesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GroupStageGamesComponent ]
     })
